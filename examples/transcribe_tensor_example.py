52f93144fda27286f7fee584429b126307051836
Ke Chen <kchen21@m.fudan.edu.cn>
2025-04-21 11:08:16 +0800

    fix resample

--- examples/transcribe_tensor_example.py
+++ examples/transcribe_tensor_example.py
@@ -14,6 +14,11 @@ from fireredasr.models.fireredasr import FireRedAsr
 def load_audio_tensor(audio_file: str):
     waveform, sample_rate = torchaudio.load(audio_file)
 
+    # 重新采样到 16000
+    if sample_rate != 16000:
+        waveform = torchaudio.functional.resample(waveform, sample_rate, 16000)
+        sample_rate = 16000
+
     # 将 float32 转换为 int16
     if waveform.dtype == torch.float32:
         waveform = (waveform * 32768.0).to(torch.int16)
@@ -28,11 +33,6 @@ def load_audio_tensor(audio_file: str):
     elif waveform.dim() == 1:
         waveform = waveform.unsqueeze(0)
 
-    # 重新采样到 16000
-    if sample_rate != 16000:
-        waveform = torchaudio.functional.resample(waveform, sample_rate, 16000)
-        sample_rate = 16000
-
     return waveform, sample_rate
 
 
@@ -41,20 +41,22 @@ def main():
     parser.add_argument("--model_dir", default="pretrained_models/FireRedASR-AED-L", help="模型目录")
     parser.add_argument("--asr_type", choices=["aed", "llm"], default="aed", help="ASR类型")
     parser.add_argument("--audio_file", default="data/test.wav", help="要转录的音频文件")
-    parser.add_argument("--use_tensor", action="store_true", default=False, help="是否使用音频张量")
-    parser.add_argument("--use_gpu", action="store_true", help="是否使用GPU")
+    parser.add_argument("--use_torchaudio", action="store_true", default=False, help="是否使用torchaudio加载音频")
     args = parser.parse_args()
 
     # 加载模型
     print(f"加载{args.asr_type}模型，路径：{args.model_dir}")
     model = FireRedAsr.from_pretrained(args.asr_type, args.model_dir)
 
-    if args.use_tensor:
+    # 如果GPU可用，则使用GPU
+    use_gpu = torch.cuda.is_available()
+
+    if args.use_torchaudio:
         waveform, sample_rate = load_audio_tensor(args.audio_file)
-        asr_args = {"use_gpu": args.use_gpu}
+        asr_args = {"use_gpu": use_gpu}
         results = model.transcribe_tensor(waveform, sample_rate, args=asr_args)
     else:
-        asr_args = {"use_gpu": args.use_gpu}
+        asr_args = {"use_gpu": use_gpu}
         results = model.transcribe(["1"], [args.audio_file], args=asr_args)
 
     # 打印结果
